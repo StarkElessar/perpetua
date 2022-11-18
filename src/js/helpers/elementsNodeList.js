d8244bcf9f2c98d8a91c7dfd431f9c0d75e8596b
VitalyChopik <leo.space97@gmail.com>
2022-11-18 17:51:16 +0300

    fix: непонятная фиксация

--- src/js/helpers/elementsNodeList.js
+++ src/js/helpers/elementsNodeList.js
@@ -1,9 +1,23 @@
 const toggleSidebarButton = document.querySelector('.burger__menu')
 const sidebar = document.querySelector('.navmenu')
 const mainContainer = document.querySelector('.main')
-  
+const boxCollection = document.querySelector('[data-collection]')
+const boxSignature = document.querySelector('[data-signature]')
+const boxConversion = document.querySelector('[data-conversion]')
+const boxAsset = document.querySelector('[data-asset]')
+const boxGeneration = document.querySelector('[data-generation]')
+const boxSpiral = document.querySelector('[data-spiral]')
+const boxCapsule = document.querySelector('[data-capsule]')
+
 export {
-  toggleSidebarButton, 
-  sidebar, 
-  mainContainer
+  toggleSidebarButton,
+  sidebar,
+  mainContainer,
+  boxCollection,
+  boxSignature,
+  boxConversion,
+  boxAsset,
+  boxGeneration,
+  boxSpiral,
+  boxCapsule,
 }
