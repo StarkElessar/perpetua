1e6b37ba95df22cfa87834e260cf0b5402cd089a
Stark <serj.elessar@gmail.com>
2022-12-02 20:06:32 +0300

    feat: замена видео-формата при изменении ширины экрана

---
+++ src/js/modules/setAdaptiveVideo.js
@@ -0,0 +1,31 @@
+const setAdaptiveVideo = () => {
+  const tagsVideo = document.querySelectorAll('[data-video]')
+
+  tagsVideo.forEach((video) => {
+    if (video.dataset.video === 'scene-1') {
+      changeVideoFormating(video)
+    } else if (video.dataset.video === 'scene-4') {
+      changeVideoFormating(video)
+    } else {
+      changeVideoFormating(video)
+    }
+  })
+}
+
+const changeVideoFormating = (parentVideNode) => {
+  const videoChildrens = parentVideNode.querySelectorAll('source')
+
+  videoChildrens.forEach((children) => {
+    if (window.innerWidth > 500) {
+      children.type === 'video/webm'
+        ? children.setAttribute('data-src', `files/video/${parentVideNode.dataset.video}.webm`)
+        : children.setAttribute('data-src', `files/video/${parentVideNode.dataset.video}.mp4`)
+    } else {
+      children.type === 'video/webm'
+        ? children.setAttribute('data-src', `files/video/mobile/${parentVideNode.dataset.video}.webm`)
+        : children.setAttribute('data-src', `files/video/mobile/${parentVideNode.dataset.video}.mp4`)
+    }
+  })
+}
+
+export default setAdaptiveVideo
