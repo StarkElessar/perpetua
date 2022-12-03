48e753e0d663bf45679875eae24e0dec64b6dca9
Сергей <serj.elessar@gmail.com>
2022-12-03 16:45:58 +0300

    fix

--- src/js/modules/sendingDataForms.js
+++ src/js/modules/sendingDataForms.js
@@ -5,21 +5,31 @@ const sendingDataForms = () => {
   document.querySelectorAll('form').forEach((form) => {
     form.addEventListener('submit', async (event) => {
       event.preventDefault()
+      const textMessage = form.querySelector('.send-message')
       
       try {
         const formData = new FormData(form)
         const {  message, result } = await postData('./../files/server.php', formData)
 
+        textMessage.style.display = 'block'
+        textMessage.innerText = 'Sending...'
+
         if (result === 'success') {
           console.log(message);
+          textMessage.innerText = message
         } else {
           console.log(message);
+          textMessage.innerText = message
         }
         
       } catch (error) {
         console.error('Error: ', error);
       } finally {
         clearInputs(form.querySelectorAll('input'))
+        setTimeout(() => {
+          textMessage.style.display = 'none'
+          textMessage.innerText = ''
+        }, 5000)
       }
     })
   })
