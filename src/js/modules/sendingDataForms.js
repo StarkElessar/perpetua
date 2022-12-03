203f3768b9de9d587d221261cd7f080cb27e612a
Сергей <serj.elessar@gmail.com>
2022-12-03 15:51:56 +0300

    Update sendingDataForms.js
    Настроил отправку данных из формы

--- src/js/modules/sendingDataForms.js
+++ src/js/modules/sendingDataForms.js
@@ -3,26 +3,24 @@ import { postData } from '../helpers/postData'
 
 const sendingDataForms = () => {
   document.querySelectorAll('form').forEach((form) => {
-    form.addEventListener('submit', (event) => {
-      const formData = new FormData(form)
-
+    form.addEventListener('submit', async (event) => {
       event.preventDefault()
+      
+      try {
+        const formData = new FormData(form)
+        const {  message, result } = await postData('./../files/mailPostNotify.php', formData)
 
-      for (const key of formData.keys()) {
-        console.log(`${key}: ${formData.get(key)}`)
+        if (result === 'success') {
+          console.log(message);
+        } else {
+          console.log(message);
+        }
+        
+      } catch (error) {
+        console.error('Error: ', error);
+      } finally {
+        clearInputs(form.querySelectorAll('input'))
       }
-
-      postData('./../files/mailPostNotify.php', formData)
-        .then((result) => {
-          alert("Thank's")
-          console.log(result)
-        })
-        .catch((error) => {
-          alert('Ошибка, попробуйте снова!', error)
-        })
-        .finally(() => {
-          clearInputs(form.querySelectorAll('input'))
-        })
     })
   })
 }
